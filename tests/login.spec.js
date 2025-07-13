2065e46de19cafb85a9356062d5a8f3cd0d7437e
Richclaud Felix <claudfelix030@gmail.com>
2025-07-14 00:25:11 +0800

    コメントを修正しました

--- tests/login.spec.js
+++ tests/login.spec.js
@@ -5,7 +5,7 @@ test('ログインが成功しました', async ({ page }) => {
 
   await page.fill('#user-name', 'standard_user'); //正しいユーザーが入力されています
   await page.fill('#password', 'secret_sauce'); //正しいパスワードが入力されています
-  await page.click('#login-button'); //ログインボタンがクリークされています
+  await page.click('#login-button'); //ログインボタンがクリックされています
 
   await expect(page).toHaveURL('https://www.saucedemo.com/inventory.html'); //URLが期待通りになったかどうか確認されています
   await expect(page.locator('.inventory_list')).toBeVisible(); //ログイン成功後、ウェブサイト内の要素がアサートされています
@@ -15,8 +15,8 @@ test('ログインが失敗しました', async ({ page }) => {
   await page.goto('https://www.saucedemo.com/'); ////ウェブサイトを開きます
 
   await page.fill('#user-name', 'locked_out_user'); //意図的に違ったユーザーが入力されています
-  await page.fill('#password', 'wrong_password'); //意図的にパスワードが入力されています
-  await page.click('#login-button'); //ログインボタンがクリークされています
+  await page.fill('#password', 'wrong_password'); //意図的に違ったパスワードが入力されています
+  await page.click('#login-button'); //ログインボタンがクリックされています
 
   await expect(page.locator('[data-test="error"]')).toContainText('Epic sadface'); //ログイン失敗のエラーメッセージがアサートされています
 });
