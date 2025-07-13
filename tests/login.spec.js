28474da2055a0ae18425aaabb5ad696191bcbc15
Richclaud Felix <claudfelix030@gmail.com>
2025-07-13 22:34:04 +0800

    テストケースにコメントを追加しました

--- tests/login.spec.js
+++ tests/login.spec.js
@@ -1,22 +1,22 @@
 const { test, expect } = require('@playwright/test');
 
-test('successful login', async ({ page }) => {
+test('ログインが成功しました', async ({ page }) => {
   await page.goto('https://www.saucedemo.com/');
 
   await page.fill('#user-name', 'standard_user');
   await page.fill('#password', 'secret_sauce');
   await page.click('#login-button');
 
-  await expect(page).toHaveURL('https://www.saucedemo.com/inventory.html');
-  await expect(page.locator('.inventory_list')).toBeVisible();
+  await expect(page).toHaveURL('https://www.saucedemo.com/inventory.html'); //URLが期待通りになったかどうかを確認されています
+  await expect(page.locator('.inventory_list')).toBeVisible(); //ウェブサイト内の要素をアサートされています
 });
 
-test('invalid login shows error message', async ({ page }) => {
+test('ログインが失敗しました', async ({ page }) => {
   await page.goto('https://www.saucedemo.com/');
 
   await page.fill('#user-name', 'locked_out_user');
   await page.fill('#password', 'wrong_password');
   await page.click('#login-button');
 
-  await expect(page.locator('[data-test="error"]')).toContainText('Epic sadface');
+  await expect(page.locator('[data-test="error"]')).toContainText('Epic sadface'); //ログイン失敗のエラーメッセージがアサートされています
 });
